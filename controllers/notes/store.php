d167512d05259e4a3854f60286f99ad93a475db5
cromone5656 <blaine.curfew@gmail.com>
2023-02-22 13:13:45 -0700

    Update hardcoded user_id

--- controllers/notes/store.php
+++ controllers/notes/store.php
@@ -7,11 +7,11 @@ use Core\Database;
 $db = App::resolve(Database::class);
 $errors = [];
 
-if (! Validator::string($_POST['body'], 1, 1000)) {
+if (!Validator::string($_POST['body'], 1, 1000)) {
     $errors['body'] = 'A body of no more than 1,000 characters is required.';
 }
 
-if (! empty($errors)) {
+if (!empty($errors)) {
     return view("notes/create.view.php", [
         'heading' => 'Create Note',
         'errors' => $errors
@@ -20,7 +20,7 @@ if (! empty($errors)) {
 
 $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
     'body' => $_POST['body'],
-    'user_id' => 1
+    'user_id' => 3
 ]);
 
 header('location: /notes');
